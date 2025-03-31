baca6fbfd2c93f124cc04a091b2febbb073e5674
victtows <josevictorjustinosilva@gmail.com>
2025-03-31 15:16:34 -0300

    refactor: :zap: Deixando o codigo mais enxuto

--- front-end/lista-tarefas/src/App.vue
+++ front-end/lista-tarefas/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Tarefas></Tarefas>
+    <Tarefas />
   </div>
 </template>
 
@@ -11,37 +11,6 @@ export default {
   name: "App",
   components: {
     Tarefas,
-  },
-  data() {
-    return {
-      tasks: [
-        {
-          id: 1,
-          title: "Learn Vue JS",
-          completed: true,
-        },
-        {
-          id: 2,
-          title: "Watch netflix",
-          completed: true,
-        },
-        {
-          id: 3,
-          title: "Go shopping",
-          completed: false,
-        },
-        {
-          id: 4,
-          title: "Learn guitar",
-          completed: false,
-        },
-        {
-          id: 5,
-          title: "Send email",
-          completed: false,
-        },
-      ],
-    };
-  },
+  }
 };
 </script>
