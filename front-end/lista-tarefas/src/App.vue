0040b6f512189389ef385e4b206142d30117fca9
victtows <josevictorjustinosilva@gmail.com>
2025-03-31 14:47:43 -0300

    feat: :zap: Adicionando conteudo no main.js

---
+++ front-end/lista-tarefas/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <Tarefas></Tarefas>
+  </div>
+</template>
+
+<script>
+import Tarefas from './components/Tarefas.vue';
+
+export default {
+  name: "App",
+  components: {
+    Tarefas,
+  },
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: "Learn Vue JS",
+          completed: true,
+        },
+        {
+          id: 2,
+          title: "Watch netflix",
+          completed: true,
+        },
+        {
+          id: 3,
+          title: "Go shopping",
+          completed: false,
+        },
+        {
+          id: 4,
+          title: "Learn guitar",
+          completed: false,
+        },
+        {
+          id: 5,
+          title: "Send email",
+          completed: false,
+        },
+      ],
+    };
+  },
+};
+</script>
